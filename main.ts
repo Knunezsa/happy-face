469684403aface86bf758bbc76c21c59b077ac66
Knunezsa <110990914+Knunezsa@users.noreply.github.com>
2022-09-07 20:06:50 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . # .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # #
+        `)
+    basic.pause(100)
+})
